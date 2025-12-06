b1de3844405e819cafaa1f249d3bf4a54c1d83c9
Asafe Orneles <asafeorneles@gmail.com>
2025-12-06 02:08:17 -0300

    init commit

---
+++ src/main/java/com/asafeorneles/gym_stock_control/GymStockControlApplication.java
@@ -0,0 +1,13 @@
+package com.asafeorneles.gym_stock_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymStockControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymStockControlApplication.class, args);
+	}
+
+}
