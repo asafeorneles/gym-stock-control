f9056a49a9da29948fb2126df809de5095884492
Asafe Orneles <asafeorneles@gmail.com>
2025-12-17 15:11:53 -0300

    docs: I documented the SaleController endpoints

--- src/main/java/com/asafeorneles/gym_stock_control/controllers/SaleController.java
+++ src/main/java/com/asafeorneles/gym_stock_control/controllers/SaleController.java
@@ -5,6 +5,9 @@ import com.asafeorneles.gym_stock_control.dtos.sale.PatchPaymentMethodDto;
 import com.asafeorneles.gym_stock_control.dtos.sale.ResponseSaleDto;
 import com.asafeorneles.gym_stock_control.queryFilters.SaleQueryFilters;
 import com.asafeorneles.gym_stock_control.services.SaleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +25,61 @@ public class SaleController {
     @Autowired
     SaleService saleService;
 
+    @Operation(summary = "Create a Sale")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Sale created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request data"),
+            @ApiResponse(responseCode = "409", description = "Conflict creating sale"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     @PostMapping
     public ResponseEntity<ResponseSaleDto> createSale(@RequestBody @Valid CreateSaleDto createSaleDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(saleService.createSale(createSaleDto));
     }
 
+    @Operation(summary = "Get all sales")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sales returned successfully"),
+            @ApiResponse(responseCode = "404", description = "Sales not found"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     @GetMapping
     public ResponseEntity<List<ResponseSaleDto>> findSales(@ParameterObject SaleQueryFilters filters){
         return ResponseEntity.status(HttpStatus.OK).body(saleService.findSales(filters.toSpecification()));
     }
 
+    @Operation(summary = "Get a sale by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sale returned successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid ID format"),
+            @ApiResponse(responseCode = "404", description = "Sale not found"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ResponseSaleDto> findSales(@PathVariable(name = "id") UUID id){
         return ResponseEntity.status(HttpStatus.OK).body(saleService.findSaleById(id));
     }
 
+    @Operation(summary = "Update the payment method of a sale")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Sale updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request data"),
+            @ApiResponse(responseCode = "404", description = "Sale not found"),
+            @ApiResponse(responseCode = "409", description = "Conflict updating sale"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     @PatchMapping("/payment-method/{id}")
     public ResponseEntity<ResponseSaleDto> updatePaymentMethod(@PathVariable(name = "id") UUID id, PatchPaymentMethodDto patchPaymentMethod){
         return ResponseEntity.status(HttpStatus.OK).body(saleService.updatePaymentMethod(id, patchPaymentMethod));
     }
 
+    @Operation(summary = "Delete a sale")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sale deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid ID format"),
+            @ApiResponse(responseCode = "404", description = "Sale not found"),
+            @ApiResponse(responseCode = "500", description = "Unexpected server error")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteSale(@PathVariable(name = "id") UUID id){
         saleService.deleteSale(id);
