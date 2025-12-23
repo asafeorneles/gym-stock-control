461f766d220f5d14ac00db9a4c0e1c09f13bb601
Asafe Orneles <asafeorneles@gmail.com>
2025-12-22 21:37:37 -0300

    refactored: I implemented size validation in UpdateCategoryDto to validate the size of the name and description.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/category/UpdateCategoryDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/category/UpdateCategoryDto.java
@@ -1,11 +1,14 @@
 package com.asafeorneles.gym_stock_control.dtos.category;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UpdateCategoryDto(
         @NotBlank(message = "The name cannot be empty!")
+        @Size(min = 2, max = 100, message = "The name should be between 2 and 100 characters")
         String name,
         @NotBlank(message = "The description cannot be empty!")
+        @Size(min = 2, max = 255, message = "The description should be between 2 and 255 characters")
         String description
 ) {
 }
