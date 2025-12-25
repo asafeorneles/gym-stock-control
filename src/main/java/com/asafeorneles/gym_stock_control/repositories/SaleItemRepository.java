27ba201b6b3543129b021a64ed72613aeae43eec
Asafe Orneles <asafeorneles@gmail.com>
2025-12-25 04:16:32 -0300

    feat: I created the SaleItemRepository

---
+++ src/main/java/com/asafeorneles/gym_stock_control/repositories/SaleItemRepository.java
@@ -0,0 +1,13 @@
+package com.asafeorneles.gym_stock_control.repositories;
+
+import com.asafeorneles.gym_stock_control.entities.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SaleItemRepository extends JpaRepository<SaleItem, UUID> {
+
+    boolean existsByProduct_id(UUID id);
+}
