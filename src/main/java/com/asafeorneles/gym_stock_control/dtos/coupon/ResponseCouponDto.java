52e0df1bee6bdde28449ef9870994ad4b9109006
Asafe Orneles <asafeorneles@gmail.com>
2025-12-26 19:49:03 -0300

    feat: I created the coupon DTOs to create and get responses.

---
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/coupon/ResponseCouponDto.java
@@ -0,0 +1,24 @@
+package com.asafeorneles.gym_stock_control.dtos.coupon;
+
+import com.asafeorneles.gym_stock_control.enums.ActivityStatus;
+import com.asafeorneles.gym_stock_control.enums.DiscountType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ResponseCouponDto(
+        UUID couponId,
+        String code,
+        String description,
+        BigDecimal discountValue,
+        DiscountType discountType,
+        int quantity,
+        ActivityStatus activityStatus,
+        LocalDateTime expirationDate
+) {
+}
