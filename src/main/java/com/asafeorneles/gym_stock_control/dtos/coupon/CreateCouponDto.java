62cad87c0fee2d985a5f32aa7e3f4fe9799ee9b1
Asafe Orneles <asafeorneles@gmail.com>
2025-12-27 20:33:12 -0300

    refactor: I added the size validations in the attributes description and reason.

--- src/main/java/com/asafeorneles/gym_stock_control/dtos/coupon/CreateCouponDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/coupon/CreateCouponDto.java
@@ -3,10 +3,7 @@ package com.asafeorneles.gym_stock_control.dtos.coupon;
 import com.asafeorneles.gym_stock_control.enums.ActivityStatus;
 import com.asafeorneles.gym_stock_control.enums.DiscountType;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ public record CreateCouponDto(
         @NotBlank(message = "The code cannot be empty!")
         String code,
         @NotBlank(message = "The description cannot be empty!")
+        @Size(min = 2, max = 255, message = "The description should be between 2 and 255 characters")
         String description,
         @NotNull(message = "The discountValue cannot be empty!")
         @DecimalMin(value = "1", message = "The min value is 1!")
--- src/main/java/com/asafeorneles/gym_stock_control/dtos/product/DeactivateProductDto.java
+++ src/main/java/com/asafeorneles/gym_stock_control/dtos/product/DeactivateProductDto.java
@@ -1,9 +1,11 @@
 package com.asafeorneles.gym_stock_control.dtos.product;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record DeactivateProductDto(
         @NotBlank(message = "The reason cannot be empty!")
+        @Size(min = 2, max = 255, message = "The reason should be between 2 and 255 characters")
         String reason
 ){
 }
