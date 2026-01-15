b469e82af605fa6939189d55848f283b1972eac1
Asafe Orneles <asafeorneles@gmail.com>
2026-01-15 18:34:01 -0300

    feat: I created an endpoint and logic for registering the first admin in the system.

---
+++ src/main/java/com/asafeorneles/gym_stock_control/configuration/security/FirstAdminDto.java
@@ -0,0 +1,4 @@
+package com.asafeorneles.gym_stock_control.configuration.security;
+
+public record FirstAdminDto (String username, String password) {
+}
--- src/main/java/com/asafeorneles/gym_stock_control/repositories/RoleRepository.java
+++ src/main/java/com/asafeorneles/gym_stock_control/repositories/RoleRepository.java
@@ -4,6 +4,9 @@ import com.asafeorneles.gym_stock_control.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String roleName);
 }
